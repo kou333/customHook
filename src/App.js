c1586da88689840ae609995c9d6a9878ff4b7d64
koustubh <55283409+kou333@users.noreply.github.com>
2024-12-16 17:24:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import UseFetchHookTest from "./components/use-fetch/test";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+    <UseFetchHookTest/> 
+    </div>
+  );
+}
+
+export default App;
